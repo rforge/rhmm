dd3f7846e27caf4b74a92bd0346c99dd591c12a2
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-23 13:42:18 +0000

    Added a file remotely
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/index.php
@@ -0,0 +1,68 @@
+<!-- This is the project specific website template -->
+<!-- It can be changed as liked or replaced by other content -->
+
+<?php
+
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='http://r-forge.r-project.org/themes/rforge/';
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?php echo $group_name; ?></title>
+	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+  </head>
+
+<body>
+
+<! --- R-Forge Logo --- >
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr><td>
+<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+</table>
+
+
+<!-- get project title  -->
+<!-- own website starts here, the following may be changed as you like -->
+
+<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
+$contents = '';
+while (!feof($handle)) {
+	$contents .= fread($handle, 8192);
+}
+fclose($handle);
+echo $contents; } ?>
+
+<!-- end of project description -->
+
+<?php 
+$mon_mess = <<<TEST
+
+RHmm package contains functions for the simulation and the estimation of Hidden Markov Models (HMM) with discrete, univariate and multivariate gaussian, univariate and multivariate mixture of gaussian conditional distributions.
+
+The main functions are:
+
+ - distributionSet and HMMSet to describe the model.
+ - HMMSim for the simulation.
+ - HMMFit for the estimation.
+ - viterbi for computing the Viterbi's path
+ - some other graphic functions.
+
+TEST;
+echo $mon_mess;
+
+ ?>
+
+
+
+<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+
+</body>
+</html>
