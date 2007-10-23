642e76eaa61d73c93ee1c7a23b927938e9c40057
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-23 12:40:36 +0000

    Removed file/folder
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/OTMathUtil.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _OTMATHUTIL_HPP_
-#define _OTMATHUTIL_HPP_
-
-#include "cOTError.hpp"
-#include "cOTVector.hpp"
-#include "cOTMAtrix.hpp"
-
-#endif //_OTMATHUTIL_HPP_
-
--- pkg/src/cOTError.cpp
+++
@@ -1,9 +0,0 @@
-#include "cOTError.hpp"
-
-cOTError::cOTError(char *theMess)
-{
-	if (theMess != (char *)NULL) 
-		std::cout << theMess << std::endl ;
-	exit(0) ;
-}
-
