5bc84f2cc5a57426866b3e62198142448a7e426b
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-11-20 12:20:38 +0000

    Added items remotely
    C:\Documents and Settings\Taram\Mes documents\R\RHmm\README
    C:\Documents and Settings\Taram\Mes documents\R\RHmm\NAMESPACE
    C:\Documents and Settings\Taram\Mes documents\R\RHmm\DESCRIPTION
    C:\Documents and Settings\Taram\Mes documents\R\RHmm\src
    C:\Documents and Settings\Taram\Mes documents\R\RHmm\R
    C:\Documents and Settings\Taram\Mes documents\R\RHmm\man
    C:\Documents and Settings\Taram\Mes documents\R\RHmm\data
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/cOTError.cpp
@@ -0,0 +1,18 @@
+/***********************************************************
+ * RHmm version 0.9.4                                      *
+ *                                                         *
+ *                                                         *
+ * Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> *
+ *                                                         *
+ * Date: 2007/11/08                                        *
+ *                                                         *
+ ***********************************************************/
+#include "cOTError.h"
+
+cOTError::cOTError(char *theMess)
+{
+	if (theMess != (char *)NULL) 
+		std::cout << theMess << std::endl ;
+	exit(0) ;
+}
+
