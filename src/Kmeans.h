f1f0d6f08c5d59e61a0bf92e6c767ae97e724681
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-28 15:31:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@157 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ src/Kmeans.h
@@ -0,0 +1,28 @@
+/**************************************************************
+ *** RHmm version 1.2.0                                      
+ ***                                                         
+ *** File: Kmeans.h 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2008/11/17                                        
+ ***                                                         
+ **************************************************************/
+
+#ifndef _KMEANS_H_
+#define _KMEANS_H_
+#ifndef _RDLL_
+
+#include "OTMathUtil.h"
+#include "REquivalents.h"
+#ifndef uint
+	typedef unsigned int uint ;
+#endif //uint
+
+
+extern void mkmeans(cOTVector& theYt, uint theNClass, int* theSeq) ;
+extern void mkmeans(cOTVector& theYt, uint theNClass, uint theDimObs, int* theSeq) ;
+#endif //_RDLL_
+#endif // _KMEANS_H_
+
+
