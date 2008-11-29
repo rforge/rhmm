f92a06095ea965339b5f31fae157d5d06d812c02
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-29 10:51:32 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@172 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/MultivariateNormalUtil.cpp
@@ -0,0 +1,50 @@
+/**************************************************************
+ *** RHmm version 1.2.0                                      
+ ***                                                         
+ *** File: MultivariateNormalUtil.cpp 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2008/11/29                                        
+ ***                                                         
+ **************************************************************/
+
+#include "MultivariateNormalUtil.h"
+
+void SymetricInverseAndDet	(	cOTMatrix&	theMat,
+								double&		theDet,
+								cOTMatrix&	theInvMat
+							)
+{
+	LapackInvAndDet(theMat, theInvMat, theDet) ;
+}
+
+
+
+void MultivariateNormalDensity	(	cOTVector&	thex,
+									cOTVector&	theMu,
+									cOTMatrix&	theInvCov,
+									double		theDet,
+									double*		theDens
+								) 
+
+{
+register uint	i,
+				j,
+				t				;
+double			myAux,
+				myRapport		;
+
+uint myDimObs = theMu.mSize ;	
+	myRapport = pow(SQRT_TWO_PI, (int)myDimObs)*sqrt(theDet) ;
+
+uint myT = thex.mSize / myDimObs ;
+
+	for ( t = 0 ; t < myT ; t++)
+	{	myAux = 0.0 ;
+		for (i = 0 ; i < myDimObs ; i++)
+			for (j = 0 ; j < myDimObs ; j++)
+				myAux += (thex[t+i*myT]-theMu[i]) * theInvCov[i][j] * (thex[t+j*myT]-theMu[j]) ;
+		theDens[t] = exp(-0.5*myAux)/myRapport ;
+	}
+}
