854de1c5135ba7e5fc4399315cb165f6ddc93a4c
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-29 10:48:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@169 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/cRandomGenerator.h
@@ -0,0 +1,25 @@
+/**************************************************************
+ *** RHmm version 1.2.0                                      
+ ***                                                         
+ *** File: cRandomGenerator.h 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2008/11/29                                        
+ ***                                                         
+ **************************************************************/
+
+#ifndef _CRANDOMGENERATOR_H_
+#define _CRANDOMGENERATOR_H_
+#ifdef _GSL_
+#include <gsl/gsl_rng.h>
+	class cRandomGenerator
+	{	public :
+			const gsl_rng_type	*mT	;
+			gsl_rng				*mr	;
+		public :
+			cRandomGenerator() ;
+			~cRandomGenerator() ;
+	} ;
+#endif // _GSL_
+#endif //_CRANDOMGENERATOR_H_
