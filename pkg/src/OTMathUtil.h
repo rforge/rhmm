59c4253b4b84fdec8c94643191993311843f6104
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-21 13:04:08 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@201 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/OTMathUtil.h
@@ -0,0 +1,19 @@
+/**************************************************************
+ *** RHmm version 1.2.1                                      
+ ***                                                         
+ *** File: OTMathUtil.h 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2009/04/21                                        
+ ***                                                         
+ **************************************************************/
+
+#ifndef _OTMathUtil_H_
+#define _OTMathUtil_H_
+
+#include "cOTError.h"
+#include "cOTVector.h"
+#include "cOTMatrix.h"
+
+#endif //_OTMathUtil_H_
