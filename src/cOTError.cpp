a47ca45f3bc5b6e7a01df123d9e50a22bf5a6507
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-27 07:08:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@208 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ src/cOTError.cpp
@@ -0,0 +1,20 @@
+/**************************************************************
+ *** RHmm version 1.3.0                                      
+ ***                                                         
+ *** File: cOTError.cpp 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2009/04/27                                       
+ ***                                                         
+ **************************************************************/
+
+#include "cOTError.h"
+
+cOTError::cOTError(char *theMess)
+{
+	if (theMess != (char *)NULL) 
+		std::cout << theMess << std::endl ;
+	exit(0) ;
+}
+
