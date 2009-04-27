59571c2a6b19b8edea5a71c68a8f7951c39c98d9
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-27 07:20:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@216 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/src/MultivariateNormalUtil.h
@@ -0,0 +1,54 @@
+/**************************************************************
+ *** RHmm version 1.3.0                                      
+ ***                                                         
+ *** File: MultivariateNormalUtil.h 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2009/04/27                                       
+ ***                                                         
+ **************************************************************/
+
+#ifndef _MULTIVARIATENORMALUTIL_H_
+#define _MULTIVARIATENORMALUTIL_H_
+#include "OTMathUtil.h"
+
+#ifndef _RDLL_
+	#include "REquivalents.h"
+#else
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <R.h>
+	#include <Rinternals.h>
+	#include <Rmath.h>
+#endif // _RDLL_
+
+
+#ifndef SQRT_TWO_PI
+	#define SQRT_TWO_PI	2.5066282746310002
+#endif //SQRT_TWO_PI
+#ifndef uint
+	typedef unsigned int uint ;
+#endif //int
+
+void SymetricInverseAndDet	(	cOTMatrix&	theMat,
+								double&		theDet,
+								cOTMatrix&	theInvMat
+							) ;
+/*void dens_normale_multivariee	(	cOTVector&	thex,
+									uint		theDimObs,	// dim de la loi multivariée
+									cOTVector&	theMu,
+									cOTMatrix&	theCov,
+									double*		theDens
+								) ;
+*/
+
+void MultivariateNormalDensity	(	cOTVector&	thex,
+									cOTVector&	theMu,
+									cOTMatrix&	theInvCov,
+									double		theDet,
+									double*		theDens
+								) ;
+
+#endif //_MULTIVARIATENORMALUTIL_H_
+
---
+++ pkg/R/src/cViterbi.h
@@ -0,0 +1,28 @@
+/**************************************************************
+ *** RHmm version 1.3.0                                      
+ ***                                                         
+ *** File: cViterbi.h 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2009/04/27                                       
+ ***                                                         
+ **************************************************************/
+
+#ifndef _CVITERBI_H_
+#define _CVITERBI_H_
+#include "cInParam.h"
+#include "cHmm.h"
+#include "cDistribution.h"
+
+class cViterbi
+{	public :
+		uint		**mSeq		;
+		cOTVector	mLogProb	;
+	public :
+		cViterbi(cInParam &theInParam) ;
+		~cViterbi() ;
+		void ViterbiPath(cInParam& theInParam, cHmm& theHMM) ;
+} ;
+
+#endif // _CVITERBI_H_
