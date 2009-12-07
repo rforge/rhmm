446c4122ed4f232d86eaf015c9ed8c798da5db28
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-07 16:15:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@284 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ rhmm/pkg/src/cHmm.h
@@ -0,0 +1,38 @@
+/**************************************************************
+ *** RHmm version 1.3.2                                      
+ ***                                                         
+ *** File: cHmm.h 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ ***                                                         
+ *** Date: 2009/12/07                                      
+ ***                                                         
+ **************************************************************/
+
+#ifndef _CHMM_H_
+#define _CHMM_H_
+#include "Hmm.h"
+#include "cInParam.h"
+
+class cDistribution ;
+class cHmm
+{	public :
+		distrDefinitionEnum	mDistrType	;
+		cOTVector			mInitProba	;
+		cOTMatrix			mTransMat	;	
+		cDistribution*		mDistrParam	;
+	public :
+		cHmm(distrDefinitionEnum theDistrType, uint theNClass , uint theDimObs=1 , uint theNMixt=0, uint theNProba=0) ;
+		cHmm(const cInParam &theInParam) ;
+		virtual ~cHmm() ;
+		cHmm & operator =(cHmm& theSrc) ;
+		void CopyHmm(cHmm& theSrc) { *this = theSrc ; } ;
+		void Print() ;
+		uint GetNParam(void) ;
+		void SetParam(cOTVector& theParam) ;
+		void GetParam(cOTVector& theParam) ;		
+} ;
+
+
+
+#endif _CHMM_H_//
