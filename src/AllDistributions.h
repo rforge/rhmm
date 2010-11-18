2a5b766c3008addd1a703b5264b819bcf2dd3de8
taram <taram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-18 13:13:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhmm@330 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ src/AllDistributions.h
@@ -0,0 +1,21 @@
+/**************************************************************
+ *** RHmm version 1.4.0                                      
+ ***                                                         
+ *** File: AllDistributions.h 
+ ***                                                         
+ *** Author: Ollivier TARAMASCO <Ollivier.Taramasco@imag.fr> 
+ *** Author: Sebastian BAUER <sebastian.bauer@charite.de>
+ *** Date: 2010/11/14                                      
+ ***                                                         
+ **************************************************************/
+
+#ifndef _ALLDISTRIBUTIONS_H_
+#define _ALLDISTRIBUTIONS_H_
+
+#include "cUnivariateNormal.h"
+#include "cMultivariateNormal.h"
+#include "cMixtUnivariateNormal.h"
+#include "cMixtMultivariateNormal.h"
+#include "cDiscrete.h"
+
+#endif// _ALLDISTRIBUTIONS_H_
